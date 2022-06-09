0fbf8521ab8ecafe3e750a53bc72c089fed61846
Lodenou <denoual.loic.dl@gmail.com>
2022-06-09 23:22:25 +0200

    Initial commit

--- app/src/main/java/com/lodenou/go4lunchv2/ui/ConnexionActivity.java
+++ app/src/main/java/com/lodenou/go4lunchv2/ui/ConnexionActivity.java
@@ -1,6 +1,8 @@
 package com.lodenou.go4lunchv2.ui;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.lodenou.go4lunchv2.R;
 
@@ -10,6 +12,10 @@ public class ConnexionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+    }
+
+    public void onClick(View v){
+
     }
 }
 
