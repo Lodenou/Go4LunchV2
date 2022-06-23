72df153bd909965d4c3da4aeb6764c970e2f1011
Lodenou <denoual.loic.dl@gmail.com>
2022-06-23 19:02:11 +0200

    Facebook & google sign in OK

--- app/src/main/java/com/lodenou/go4lunchv2/ui/ConnexionActivity.java
+++ app/src/main/java/com/lodenou/go4lunchv2/ui/ConnexionActivity.java
@@ -1,21 +1,233 @@
 package com.lodenou.go4lunchv2.ui;
 
-import android.os.Bundle;
-import android.view.View;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.lodenou.go4lunchv2.R;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.lodenou.go4lunchv2.databinding.ActivityConnexionBinding;
+import com.facebook.FacebookSdk;
+
+import java.util.Arrays;
+
 
 public class ConnexionActivity extends AppCompatActivity {
+
+    private ActivityConnexionBinding binding;
+    private FirebaseAuth mAuth;
+    private final String TAG = "GOOGLE_SIGN_IN_TAG";
+    private final int RC_SIGN_IN = 123;
+    private GoogleSignInClient mGoogleSignInClient;
+    private CallbackManager mCallbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connexion);
+        binding = ActivityConnexionBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        mAuth = FirebaseAuth.getInstance();
+        setGoogleSignIn();
+        facebookSignIn();
+
     }
 
-    public void onClick(View v){
+    @Override
+    protected void onStart() {
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        super.onStart();
+    }
+
+    public void onClick(View v) {
+        if (v == binding.googleButton) {
+            Log.d(TAG, "OnClick: begin Google SignIn");
+            Intent intent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(intent, RC_SIGN_IN);
+
+        }
+        if (v == binding.loginButtonFb) {
+
+            binding.loginButtonFb.performClick();
+        }
+    }
+
+    // GOOGLE LOGIN
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // GOOGLE
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> mAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = mAccountTask.getResult(ApiException.class);
+                FirebaseAuthWithGoogleAccount(account);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Log.d(TAG, "OnClick: ERROR ACTIVITY RESULT NOT OK");
+
+        }
+        // FACEBOOK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void FirebaseAuthWithGoogleAccount(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseauthwithgoogleaccount: begin firebase auth with google account");
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Log.d(TAG, "OnSuccess: Logged in");
+                        // Get logged in user
+                        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                        // Get user info
+                        String uid = firebaseUser.getUid();
+                        String email = firebaseUser.getEmail();
+                        Log.d(TAG, "OnSuccess: Email" + email);
+                        Log.d(TAG, "OnSuccess: uid" + uid);
+                        // Check if user is new or existing
+                        if (authResult.getAdditionalUserInfo().isNewUser()) {
+                            // User is new account created
+                            Log.d(TAG, "OnSuccess: Account created...\n" + email);
+                            Toast.makeText(ConnexionActivity.this, "Account created...\n" + email, Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Existing user logged in
+                            Log.d(TAG, "OnSuccess: Existing user... \n" + email);
+                            Toast.makeText(ConnexionActivity.this, "Existing user...\n" + email, Toast.LENGTH_SHORT).show();
+                        }
+
+                        // Start MainActivity
+                        startActivity(new Intent(ConnexionActivity.this, MainActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "OnFailure: Loggin failed" + e.getMessage());
+                    }
+                });
+    }
+
+    private void setGoogleSignIn() {
+        // Configure google sign in
+        GoogleSignInOptions mGoogleSignInOption = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("859453280997-nrjn57ulqiao9aer8bckq3hdg2lbtlv0.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOption);
+
+        // Init firebase auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void facebookSignIn() {
+        mCallbackManager = CallbackManager.Factory.create();
+        binding.loginButtonFb.setReadPermissions("email", "public_profile");
+        binding.loginButtonFb.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
 
     }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:" + accessToken);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(ConnexionActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
+
+    FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                //Start main activity
+                startActivity(new Intent(ConnexionActivity.this, MainActivity.class));
+                Toast.makeText(ConnexionActivity.this,
+                        "You successfully signed-in ", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    };
+
+
+    private void updateUI(FirebaseUser user) {
+
+        if (user != null) {
+            Toast.makeText(this, "You Signed In successfully", Toast.LENGTH_LONG).show();
+
+        } else {
+            Toast.makeText(this, "You Didnt signed in", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
 
--- app/src/main/java/com/lodenou/go4lunchv2/ui/MainActivity.java
+++ app/src/main/java/com/lodenou/go4lunchv2/ui/MainActivity.java
@@ -1,14 +1,266 @@
 package com.lodenou.go4lunchv2.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lodenou.go4lunchv2.R;
+import com.lodenou.go4lunchv2.databinding.ActivityMainBinding;
+import com.lodenou.go4lunchv2.databinding.NavHeaderMainBinding;
+import com.lodenou.go4lunchv2.service.PageAdapter;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
+//    private NavHeaderMainBinding navViewBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // view binding
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Add binding for nav_view
+//         navViewBinding = NavHeaderMainBinding.bind(binding.navView);
+//
+//        View view = binding.getRoot();
         setContentView(R.layout.activity_main);
+//        setContentView(view);
+        mAuth = FirebaseAuth.getInstance();
+        checkUser();
+        createNavMenu();
+        configureViewPagerAndTabs();
+        setTabIcons();
+        setNavMenuOnClicks();
     }
+
+    private void checkUser() {
+        // get current user
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        View headerView = ((NavigationView) findViewById(R.id.nav_view))
+                .getHeaderView(0);
+        if (firebaseUser == null) {
+            // User not logged in
+            startActivity(new Intent(this, ConnexionActivity.class));
+            finish();
+        }
+        // User logged in
+        // Get user info
+        else {
+            String email = firebaseUser.getEmail();
+            TextView navUserMail = headerView
+                    .findViewById(R.id.nav_user_mail);
+            navUserMail.setText(email);
+            Toast.makeText(this, email, Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    // 4 - Declare Subscription
+    private Disposable disposable;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+    // -----------------
+    // ACTIONS
+    // -----------------
+
+
+//        this.streamShowString();
+
+
+    // ------------------------------
+    //  Reactive X
+    // ------------------------------
+
+    // 1 - Create Observable
+    private Observable<String> getObservable() {
+        return Observable.just("Cool !");
+    }
+
+    // 2 - Create Subscriber
+
+    private DisposableObserver<String> getSubscriber() {
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String item) {
+//                textView.setText("Observable emits : "+item);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("TAG", "On Error" + Log.getStackTraceString(e));
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("TAG", "On Complete !!");
+            }
+        };
+    }
+
+    // 3 - Create Stream and execute it
+    private void streamShowString() {
+        this.disposable = this.getObservable()
+                .subscribeWith(getSubscriber());
+    }
+
+    // 5 - Dispose subscription
+    private void disposeWhenDestroy() {
+        if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
+    }
+
+
+    // SET UI
+
+    private void createNavMenu() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+        };
+    }
+
+    private void setNavMenuOnClicks() {
+        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_yourlunch:
+//                        mUser = new User();
+////                        FirebaseUser userf = FirebaseAuth.getInstance().getCurrentUser();
+//                        String id = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+//                        UserHelper.getUser(id).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                            @Override
+//                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                                mUser = documentSnapshot.toObject(User.class);
+//
+//                                if (mUser.getRestaurantPlaceId() != null && mUser.getRestaurantPlaceId() != "") {
+//                                    Intent intent = new Intent(MainActivity.this, YourLunchActivity.class);
+//                                    intent.putExtra("key", mUser.getRestaurantPlaceId());
+//                                    startActivity(intent);
+//                                } else {
+//                                    new AlertDialog.Builder(MainActivity.this)
+//                                            .setTitle("Oops")
+//                                            .setMessage("You didn't chose a restaurant !")
+//                                            .show();
+//                                }
+//                            }
+//                        });
+                        return true;
+                    case R.id.nav_settings:
+                        Intent intent2 = new Intent(MainActivity.this, SettingsActivity.class);
+                        startActivity(intent2);
+                        return true;
+                    case R.id.nav_logout:
+                        logOut();
+                        return true;
+                    case R.id.chat:
+                        Intent i = new Intent(MainActivity.this, ChatActivity.class);
+                        startActivity(i);
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    private void configureViewPagerAndTabs() {
+
+        ViewPager pager = (ViewPager) findViewById(R.id.activity_main_viewpager);
+        //Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager()));
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tab_layout);
+        // Glue TabLayout and ViewPager together
+        tabs.setupWithViewPager(pager);
+        // Design purpose. Tabs have the same width
+        tabs.setTabMode(TabLayout.MODE_FIXED);
+        final ActionBar toolbar = getSupportActionBar();
+        toolbar.setTitle("I'm hungry !");
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        toolbar.setTitle("I'm hungry !");
+                        break;
+                    case 1:
+                        toolbar.setTitle("I'm hungry !");
+                        break;
+                    case 2:
+                        toolbar.setTitle("Available workmates");
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void setTabIcons() {
+        int[] tabIcons = {
+                R.drawable.ic_baseline_map_24,
+                R.drawable.ic_baseline_view_list_24,
+                R.drawable.ic_baseline_people_24
+        };
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+    }
+
+    private void logOut() {
+
+        // FIREBASE LOGOUT
+        FirebaseAuth.getInstance().signOut();
+        // GOOGLE LOGOUT
+        GoogleSignInOptions gso = new GoogleSignInOptions.
+                Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
+                build();
+
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
+        googleSignInClient.signOut();
+        Intent intent = new Intent(MainActivity.this, ConnexionActivity.class);
+        finish();
+        startActivity(intent);
+        // FACEBOOK LOGOUT
+        LoginManager.getInstance().logOut();
+    }
+
+
 }
